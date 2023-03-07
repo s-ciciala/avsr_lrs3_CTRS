e2f2af7820ad195d9ca143ce19aaec2dafc4b2e0
SzymonCiciala <47600083+s-ciciala@users.noreply.github.com>
2023-03-07 19:43:37 +0000

    adding preprocess file walking

--- audio_only/data/utils.py
+++ audio_only/data/utils.py
@@ -21,12 +21,14 @@ def prepare_main_input(audioFile, targetFile, noise, reqInpLen, charToIx, noiseS
     """
 
     if targetFile is not None:
-        print("FILE")
-        print(targetFile)
+        # print("FILE")
+        # print(targetFile)
         #reading the target from the target file and converting each character to its corresponding index
         with open(targetFile, "r") as f:
             trgt = f.readline().strip()[7:]
-        print(trgt)
+        if "{" in trgt:
+            trgt = trgt.split("{")[0]
+        # print(trgt)
         trgt = [charToIx[char] for char in trgt]
         trgt.append(charToIx["<EOS>"])
         trgt = np.array(trgt)
