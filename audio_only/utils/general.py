70889ca163626145be348799f7d3dca9459e479b
s-ciciala <s1834237@ed.ac.uk>
2023-03-19 19:31:09 +0000

    test preproc

--- audio_only/utils/general.py
+++ audio_only/utils/general.py
@@ -162,7 +162,9 @@ def evaluate(model, evalLoader, loss_function, device, evalParams):
         for i in range(predictionBatch.shape[0]):
             predictionString = ""
             for j in range(predictionLenBatch[i]):
-                print(predictionBatch[i][j])
+                print(predictionBatch[i])
+                print(predictionBatch[i][j].item())
+                exit()
                 # predictionString += index_to_char[predictionBatch[i][j].item()]
             # predictionStrings.append(predictionString)
 
