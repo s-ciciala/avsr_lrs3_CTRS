e18d5d59ca5f91d6fd5aa24e76e5f1697fa598cf
s-ciciala <s1834237@ed.ac.uk>
2023-03-19 17:51:48 +0000

    test preproc

--- audio_only/models/audio_net.py
+++ audio_only/models/audio_net.py
@@ -37,19 +37,7 @@ class PositionalEncoding(nn.Module):
         return outputBatch
 
 
-
 class AudioNet(nn.Module):
-
-    """
-    An audio-only speech transcription model based on the Transformer architecture.
-    Architecture: A stack of 12 Transformer encoder layers,
-                  first 6 form the Encoder and the last 6 form the Decoder.
-    Character Set: 26 alphabets (A-Z), 10 numbers (0-9), apostrophe ('), space ( ), blank (-), end-of-sequence (<EOS>)
-    Input: 321-dim STFT feature vectors with 100 vectors per second. Each group of 4 consecutive feature vectors
-           is linearly transformed into a single 512-dim feature vector giving 25 vectors per second.
-    Output: Log probabilities over the character set at each time step.
-    """
-
     def __init__(self, dModel, nHeads, numLayers, peMaxLen, inSize, fcHiddenSize, dropout, numClasses):
         super(AudioNet, self).__init__()
         self.audioConv = nn.Conv1d(inSize, dModel, kernel_size=4, stride=4, padding=0)
@@ -57,8 +45,44 @@ class AudioNet(nn.Module):
         encoderLayer = nn.TransformerEncoderLayer(d_model=dModel, nhead=nHeads, dim_feedforward=fcHiddenSize, dropout=dropout)
         self.audioEncoder = nn.TransformerEncoder(encoderLayer, num_layers=numLayers)
         self.audioDecoder = nn.TransformerEncoder(encoderLayer, num_layers=numLayers)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
         self.outputConv = nn.Conv1d(dModel, numClasses, kernel_size=1, stride=1, padding=0)
-        return
+
+    def forward(self, inputBatch):
+        inputBatch = inputBatch.transpose(0, 1).transpose(1, 2)
+        batch = self.audioConv(inputBatch)
+        batch = batch.transpose(1, 2).transpose(0, 1)
+        batch = self.positionalEncoding(batch)
+        batch = self.audioEncoder(batch)
+        batch = self.audioDecoder(batch)
+        batch = batch.transpose(0, 1).transpose(1, 2)
+        batch = self.pool(batch)
+        batch = self.outputConv(batch)
+        batch = batch.transpose(1, 2).transpose(0, 1)
+        outputBatch = F.log_softmax(batch, dim=2)
+        return outputBatch
+
+# class AudioNet(nn.Module):
+#
+#     """
+#     An audio-only speech transcription model based on the Transformer architecture.
+#     Architecture: A stack of 12 Transformer encoder layers,
+#                   first 6 form the Encoder and the last 6 form the Decoder.
+#     Character Set: 26 alphabets (A-Z), 10 numbers (0-9), apostrophe ('), space ( ), blank (-), end-of-sequence (<EOS>)
+#     Input: 321-dim STFT feature vectors with 100 vectors per second. Each group of 4 consecutive feature vectors
+#            is linearly transformed into a single 512-dim feature vector giving 25 vectors per second.
+#     Output: Log probabilities over the character set at each time step.
+#     """
+#
+#     def __init__(self, dModel, nHeads, numLayers, peMaxLen, inSize, fcHiddenSize, dropout, numClasses):
+#         super(AudioNet, self).__init__()
+#         self.audioConv = nn.Conv1d(inSize, dModel, kernel_size=4, stride=4, padding=0)
+#         self.positionalEncoding = PositionalEncoding(dModel=dModel, maxLen=peMaxLen)
+#         encoderLayer = nn.TransformerEncoderLayer(d_model=dModel, nhead=nHeads, dim_feedforward=fcHiddenSize, dropout=dropout)
+#         self.audioEncoder = nn.TransformerEncoder(encoderLayer, num_layers=numLayers)
+#         self.audioDecoder = nn.TransformerEncoder(encoderLayer, num_layers=numLayers)
+#         self.outputConv = nn.Conv1d(dModel, numClasses, kernel_size=1, stride=1, padding=0)
+#         return
 
 
     # def forward(self, inputBatch):
@@ -73,21 +97,21 @@ class AudioNet(nn.Module):
     #     batch = batch.transpose(1, 2).transpose(0, 1)
     #     outputBatch = F.log_softmax(batch, dim=2)
     #     return outputBatch
-    def forward(self, inputBatch):
-        inputLen = inputBatch.shape[0]
-        # Calculate the number of zero-padding needed
-        padLen = 4 - (inputLen % 4)
-        # Add zero-padding to the input batch
-        inputBatch = torch.cat([inputBatch, torch.zeros(padLen, inputBatch.shape[1])], dim=0)
-        inputBatch = inputBatch.transpose(0, 1).transpose(1, 2)
-        batch = self.audioConv(inputBatch)
-        batch = batch.transpose(1, 2).transpose(0, 1)
-        batch = self.positionalEncoding(batch)
-        batch = self.audioEncoder(batch)
-        batch = self.audioDecoder(batch)
-        batch = batch.transpose(0, 1).transpose(1, 2)
-        batch = self.outputConv(batch)
-        batch = batch.transpose(1, 2).transpose(0, 1)
-        # Remove the zero-padding from the output batch
-        outputBatch = F.log_softmax(batch[:inputLen, :, :], dim=2)
-        return outputBatch
+    # def forward(self, inputBatch):
+    #     inputLen = inputBatch.shape[0]
+    #     # Calculate the number of zero-padding needed
+    #     padLen = 4 - (inputLen % 4)
+    #     # Add zero-padding to the input batch
+    #     inputBatch = torch.cat([inputBatch, torch.zeros(padLen, inputBatch.shape[1])], dim=0)
+    #     inputBatch = inputBatch.transpose(0, 1).transpose(1, 2)
+    #     batch = self.audioConv(inputBatch)
+    #     batch = batch.transpose(1, 2).transpose(0, 1)
+    #     batch = self.positionalEncoding(batch)
+    #     batch = self.audioEncoder(batch)
+    #     batch = self.audioDecoder(batch)
+    #     batch = batch.transpose(0, 1).transpose(1, 2)
+    #     batch = self.outputConv(batch)
+    #     batch = batch.transpose(1, 2).transpose(0, 1)
+    #     # Remove the zero-padding from the output batch
+    #     outputBatch = F.log_softmax(batch[:inputLen, :, :], dim=2)
+    #     return outputBatch
