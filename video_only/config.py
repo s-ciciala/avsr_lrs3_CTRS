b5bd66c918d4043243f4bb971e9773249d98d3c0
s-ciciala <s1834237@ed.ac.uk>
2023-03-21 11:49:11 +0000

    train video

--- video_only/config.py
+++ video_only/config.py
@@ -80,18 +80,18 @@ args["NUM_CLASSES"] = 40    #number of output characters
 
 
 #transformer architecture
-# args["PE_MAX_LENGTH"] = 2500    #length up to which we calculate positional encodings
-# args["TX_NUM_FEATURES"] = 512   #transformer input feature size
-# args["TX_ATTENTION_HEADS"] = 8  #number of attention heads in multihead attention layer
-# args["TX_NUM_LAYERS"] = 6   #number of Transformer Encoder blocks in the stack
-# args["TX_FEEDFORWARD_DIM"] = 2048   #hidden layer size in feedforward network of transformer
-# args["TX_DROPOUT"] = 0.1    #dropout probability in the transformer
 args["PE_MAX_LENGTH"] = 2500    #length up to which we calculate positional encodings
 args["TX_NUM_FEATURES"] = 512   #transformer input feature size
-args["TX_ATTENTION_HEADS"] = 4  #number of attention heads in multihead attention layer
-args["TX_NUM_LAYERS"] = 3   #number of Transformer Encoder blocks in the stack
-args["TX_FEEDFORWARD_DIM"] = 1024   #hidden layer size in feedforward network of transformer
+args["TX_ATTENTION_HEADS"] = 8  #number of attention heads in multihead attention layer
+args["TX_NUM_LAYERS"] = 6   #number of Transformer Encoder blocks in the stack
+args["TX_FEEDFORWARD_DIM"] = 2048   #hidden layer size in feedforward network of transformer
 args["TX_DROPOUT"] = 0.1    #dropout probability in the transformer
+# args["PE_MAX_LENGTH"] = 2500    #length up to which we calculate positional encodings
+# args["TX_NUM_FEATURES"] = 512   #transformer input feature size
+# args["TX_ATTENTION_HEADS"] = 4  #number of attention heads in multihead attention layer
+# args["TX_NUM_LAYERS"] = 3   #number of Transformer Encoder blocks in the stack
+# args["TX_FEEDFORWARD_DIM"] = 1024   #hidden layer size in feedforward network of transformer
+# args["TX_DROPOUT"] = 0.1    #dropout probability in the transformer
 
 
 #beam search
