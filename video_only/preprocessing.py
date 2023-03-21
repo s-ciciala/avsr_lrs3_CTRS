6205e4b2edeeb5d789e9d75313b7c18f93a221c1
s-ciciala <s1834237@ed.ac.uk>
2023-03-21 11:33:54 +0000

    {} still found in training so much redo sets

--- video_only/preprocessing.py
+++ video_only/preprocessing.py
@@ -13,6 +13,14 @@ from sklearn.model_selection import train_test_split
 import collections
 
 
+def lrs3_parse(example):
+    splt = example.split("{")
+    print("Had to split")
+    print("Was " + str(example))
+    print("Is " + str(splt))
+    return splt[0]
+
+
 def set_device():
     np.random.seed(args["SEED"])
     torch.manual_seed(args["SEED"])
@@ -171,6 +179,8 @@ def generate_train_file(train):
             # print(examples_npy_dir)
             example_dict["ID"].append(examples_npy_dir)
             string_to_add = str(lines[0][6: -1])
+            if "{" in string_to_add:
+                string_to_add = lrs3_parse(string_to_add)
             # print(string_to_add)
             example_dict["TEXT"].append(string_to_add)
             # print(example_dict)
@@ -201,6 +211,8 @@ def generate_val_file(val):
             # print(examples_npy_dir)
             example_dict["ID"].append(examples_npy_dir)
             string_to_add = str(lines[0][6: -1])
+            if "{" in string_to_add:
+                string_to_add = lrs3_parse(string_to_add)
             # print(string_to_add)
             example_dict["TEXT"].append(string_to_add)
             # print(example_dict)
@@ -232,6 +244,8 @@ def generate_test_file(test):
             # print(examples_npy_dir)
             example_dict["ID"].append(examples_npy_dir)
             string_to_add = str(lines[0][6: -1])
+            if "{" in string_to_add:
+                string_to_add = lrs3_parse(string_to_add)
             # print(string_to_add)
             example_dict["TEXT"].append(string_to_add)
             # print(example_dict)
@@ -280,8 +294,8 @@ if __name__ == "__main__":
     print("Size of val set:" + str(len(val)))
     print("Size of test set:" + str(len(test)))
     # preprocess_all_samples(fileList,device)
-    # generate_train_file(train)
-    # generate_val_file(val)
+    generate_train_file(train)
+    generate_val_file(val)
     generate_test_file(test)
     check_files_correct_len(train, val, test)
     print("Completed")
