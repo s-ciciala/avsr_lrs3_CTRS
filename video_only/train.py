818428534ff2a857a47fadee16279abcce467be2
s-ciciala <s1834237@ed.ac.uk>
2023-03-21 11:46:17 +0000

    train video

--- video_only/train.py
+++ video_only/train.py
@@ -55,6 +55,12 @@ def get_training_data(device, kwargs):
 
     model = VideoNet(args["TX_NUM_FEATURES"], args["TX_ATTENTION_HEADS"], args["TX_NUM_LAYERS"], args["PE_MAX_LENGTH"],
                      args["TX_FEEDFORWARD_DIM"], args["TX_DROPOUT"], args["NUM_CLASSES"])
+
+    if args["LIMITGPU"]:
+        model = nn.DataParallel(model, device_ids=args["GPUID"])
+    else:
+        model = nn.DataParallel(model)
+
     model.to(device)
     return trainData, trainLoader, valData, valLoader, model
 
